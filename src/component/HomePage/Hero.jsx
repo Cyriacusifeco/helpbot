eb0b3204a9be384c42ddaff582444652064a5278
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-08-12 18:08:17 +0100

    renamed to Hero

---
+++ src/component/HomePage/Hero.jsx
@@ -0,0 +1,34 @@
+import './Hero.css'
+
+
+
+
+export default function Startup() {
+  return (
+    <>
+    
+      <div className="container h-50">
+        <div className='row'>
+          <div className='col-sm-6 hero-container pt-4 p-2'>
+            <div>
+            <h1 className='heading-1 p-2  text-wrap'>
+              Enhance Your Customer Service with a Personalized
+              HelpBot!
+            </h1>
+           
+            <h4 className='p-2 pb-3 lh-sm text-wrap'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ac lorem nibh. 
+            </h4>
+            <div className='startup p-2'>
+              <button className='active'>Build your Chatbot</button>
+              <button className='inactive shadow'>Learn more</button>
+            </div>
+            </div>
+ 
+          </div>
+          
+        </div>
+      </div>
+    </>
+  )
+}
