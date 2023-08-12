feab0a01b8e8171659d56280f9324447e871096f
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-08-12 17:41:13 +0100

    created component folder
    Moved components into their respective folders

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from './NavBar';
-import Startup from './Startup';
-import Signup from './Signup';
+import NavBar from '../NavBar/NavBar';
+import Startup from '../Startup/Startup';
+import Signup from '../Auth/Signup';
 import './App.css';
 
 export default function App() {
--- src/component/Auth/Signup.jsx
+++ src/component/Auth/Signup.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import '../App/App.css'
 import './Signup.css'
 
 
