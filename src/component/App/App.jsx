5964def23ccc7af925a3b785fa007d2c3b0f0ecf
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-08-12 20:00:32 +0100

    Update App.jsx

--- src/component/App/App.jsx
+++ src/component/App/App.jsx
@@ -1,22 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavBar from '../NavBar/NavBar';
-import Signup from '../Auth/Signup';
+import NavBar from '../NavBar/NavBar'
+import Signup from '../Auth/Signup'
+import SignIn from '../Auth/SignIn'
 import Home from '../HomePage/Home'
 import './App.css';
 
 export default function App() {
   return (
-    <>
+    
     <Router>
       <div>
         <NavBar />
         <Routes>
           <Route path="/" element={<Home/>} />
-          <Route  path="/About" element={<Signup />} />
+          <Route path="/About" element={<Signup />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </Router>
-    </>
+    
   );
 }
