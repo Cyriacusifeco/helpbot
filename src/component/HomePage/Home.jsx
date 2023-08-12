8ed59038f89bf23bdaabb791eccb74c39b9c7339
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-08-12 18:26:38 +0100

    Create Home.jsx

---
+++ src/component/HomePage/Home.jsx
@@ -0,0 +1,16 @@
+import Hero from './Hero'
+import './Hero.css'
+
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <div>
+        <h1>hello</h1>
+      </div>
+    </>
+  )
+}
