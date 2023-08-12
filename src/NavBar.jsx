fe3afdec9a7c1d16727016429b865099420c135e
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-08-12 17:20:37 +0100

    Update NavBar.jsx

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 
-=======
 import './App/App.css';
->>>>>>> 77921f87a9c2b85c7da730d8d59d6ece253ad86a
-
 import { Link } from 'react-router-dom';
 
 import './App.css';
@@ -50,61 +46,5 @@ export default function NavBar() {
         </div>
       </div>
     </nav>
-=======
-import './components/App/App.css';
-
-export default function NavBar() {
-  return (
-    <>
-      <nav className="navbar fixed-top navbar-expand-lg border-bottom border-body m-3">
-        <div className="container-fluid">
-          <a className="navbar-brand logo  " href="#"></a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarText"
-            aria-controls="navbarText"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item ">
-                <a className="nav-link" aria-current="page" href="#">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Demo
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Features
-                </a>
-              </li>
-            </ul>
-            <span className="navbar-text">
-              <button className="btn-2 me-3" type="submit">
-                Try demo
-              </button>
-              <button className="btn-1" type="submit">
-                Get started
-              </button>
-            </span>
-          </div>
-        </div>
-      </nav>
-    </>
->>>>>>> 2afacec901d98f5915220b751254fdb051e954fb
   );
 }
