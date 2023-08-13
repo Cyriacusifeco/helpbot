e323dd4a082bef357b17f4515f0062417a037b01
Udo Chinagwam <111175597+udobeke@users.noreply.github.com>
2023-08-13 20:31:44 +0100

    Update Admin.jsx

--- src/component/Dashboard/NavBar/Admin.jsx
+++ src/component/Dashboard/NavBar/Admin.jsx
@@ -8,76 +8,78 @@ export default function Admin() {
   return (
     <>
       <div className='container'>
-      <div className="sidebar">
-        <div className="logo-details row">
-          <i className="bx bxl-c-plus-plus" />
-          <span className="logo_name">LOGO</span>
+        <div className='row'>
+          <div className="col-md-2 sidebar">
+            <div className="logo-details row">
+              <i className="bx bxl-c-plus-plus" />
+              <span className="logo_name">LOGO</span>
+            </div>
+            <ul className="nav-links">
+              <li>
+                <a href="#" className="active">
+                  <i className="bx bx-grid-alt" />
+                  <span className="links_name">Dashboard</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="bx bx-box" />
+                  <span className="links_name">Product</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="bx bx-list-ul" />
+                  <span className="links_name">Order list</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="bx bx-pie-chart-alt-2" />
+                  <span className="links_name">Analytics</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="bx bx-coin-stack" />
+                  <span className="links_name">Stock</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="bx bx-book-alt" />
+                  <span className="links_name">Total order</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="bx bx-user" />
+                  <span className="links_name">Team</span>
+                </a>
+              </li>
+
+              <li>
+                <a href="#">
+                  <i className="bx bx-heart" />
+                  <span className="links_name">Favrorites</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <i className="bx bx-cog" />
+                  <span className="links_name">Setting</span>
+                </a>
+              </li>
+              <li className="log_out">
+                <a href="#">
+                  <i className="bx bx-log-out" />
+                  <span className="links_name">Log out</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        <ul className="nav-links">
-          <li>
-            <a href="#" className="active">
-              <i className="bx bx-grid-alt" />
-              <span className="links_name">Dashboard</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="bx bx-box" />
-              <span className="links_name">Product</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="bx bx-list-ul" />
-              <span className="links_name">Order list</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="bx bx-pie-chart-alt-2" />
-              <span className="links_name">Analytics</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="bx bx-coin-stack" />
-              <span className="links_name">Stock</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="bx bx-book-alt" />
-              <span className="links_name">Total order</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="bx bx-user" />
-              <span className="links_name">Team</span>
-            </a>
-          </li>
-          
-          <li>
-            <a href="#">
-              <i className="bx bx-heart" />
-              <span className="links_name">Favrorites</span>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="bx bx-cog" />
-              <span className="links_name">Setting</span>
-            </a>
-          </li>
-          <li className="log_out">
-            <a href="#">
-              <i className="bx bx-log-out" />
-              <span className="links_name">Log out</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <section className="home-section">
+        <section className="home-section">
           <Navbar bg="light" data-bs-theme="light">
             <Container>
               <Navbar.Brand href="#home">Navbar</Navbar.Brand>
@@ -88,9 +90,9 @@ export default function Admin() {
               </Nav>
             </Container>
           </Navbar>
-        
-            
-          
+
+
+
         </section>
       </div>
     </>
