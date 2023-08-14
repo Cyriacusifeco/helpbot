236a73de22aab23a0c099a1fbd0d9a74d840069a
Charly <100117263+CharlyAnne@users.noreply.github.com>
2023-08-14 02:04:20 +0100

    Update Home.jsx

--- src/component/HomePage/Home.jsx
+++ src/component/HomePage/Home.jsx
@@ -1,9 +1,6 @@
 import NavBar from './NavBar/Navbar';
 import Hero from './Hero/Hero';
-<<<<<<< HEAD
-=======
 import '../Hero/Hero.css';
->>>>>>> 5d516bcbc94b56a59e0b56c6aa87f170dd5080c7
 
 export default function Home() {
   return (
