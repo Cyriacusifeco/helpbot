0e5f4529318d7b0c434fe273c77a1ead22193b46
Charly <icharlieanne@gmail.com>
2023-08-25 00:09:34 +0100

    Auth pages

--- src/component/Auth/LogIn.jsx
+++ src/component/Auth/LogIn.jsx
@@ -8,7 +8,6 @@ import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 const LogIn = () => {
   const {
     register,
-    handleSubmit,
     formState: { errors },
   } = useForm();
   return (
@@ -23,18 +22,10 @@ const LogIn = () => {
           <img src="src/assets/2.jpeg" alt="" className="img-fluid image" />
         </div>
         <div className="col-md-6 log-in p-3">
-          <form onSubmit={handleSubmit} className="sign-in-form">
+          <form className="sign-in-form">
             <h2 className="title">Welcome back! </h2>
             <p className="subtitle">Please enter your credentials</p>
-            {/* <div className="input-field">
-              <span className="fIcon">
-                <FontAwesomeIcon icon={faUser} />
-              </span>
-              <input
-                placeholder="Name"
-                {...register('name', { required: true })}
-              />
-            </div> */}
+
             <div className="input-field">
               <span className="fIcon">
                 <FontAwesomeIcon icon={faEnvelope} />
@@ -68,42 +59,3 @@ const LogIn = () => {
   );
 };
 export default LogIn;
-
-// export default function Signup() {
-//   return (
-//     <div className='container mt-5'>
-//       <div className='row'>
-//         <div className='col-sm-6 p-3 d-none  d-sm-block'>
-//           <img src="src/assets/2.jpeg" alt="" className='img-fluid image' />
-//         </div>
-//         <div className='col-md-6 log-in p-3'>
-//           <div className="logo-container">
-//             <a href="#">LOGO</a>
-//           </div>
-//           <h3>Log In</h3>
-//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, consectetur adipiscing elit </p>
-//           <div className="mb-3">
-//             <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email address" />
-//           </div>
-//           <div className="mb-3">
-//             <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Password" />
-//           </div>
-//           <div className="mb-3">
-//             <button className='active'><FontAwesomeIcon icon={faEnvelope} /> Log In</button>
-//           </div>
-//           <hr />
-//           <div className="mb-2">
-//             <button className='inactive'>Log In with Google</button>
-//           </div>
-//           <div className="mb-2">
-//             <button className='inactive'><FontAwesomeIcon icon={faEnvelope} /> Log in with Apple</button>
-//           </div>
-//           <p>Forgot your password</p>
-//           <p>Don&apos;t have an account <Link to="/SignUp">Sign Up</Link></p>
-//         </div>
-
-//       </div>
-
-//     </div>
-//   )
-// }
--- src/component/Auth/Signup.jsx
+++ src/component/Auth/Signup.jsx
@@ -9,7 +9,6 @@ import { faEnvelope, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
 const Signup = () => {
   const {
     register,
-    handleSubmit,
     formState: { errors },
   } = useForm();
   return (
@@ -24,7 +23,11 @@ const Signup = () => {
           <img src="src/assets/2.jpeg" alt="" className="img-fluid image" />
         </div>
         <div className="col-md-6 log-in p-3">
-          <form onSubmit={handleSubmit} className="sign-up-form">
+          <form
+            method="POST"
+            action="/api/business/register"
+            className="sign-up-form"
+          >
             <h2 className="title">Sign Up</h2>
             <p className="subtitle">It is quick and easy.</p>
             <div className="input-field">
@@ -69,54 +72,3 @@ const Signup = () => {
   );
 };
 export default Signup;
-
-//     <div className="container mt-5">
-//       <div className="row">
-//         <div className="col-sm-6 p-3 d-none  d-sm-block">
-//           <img src="src/assets/2.jpeg" alt="" className="img-fluid image" />
-//         </div>
-//         <div className="col-md-6 log-in p-3">
-//           <div className="logo-container">
-//             <a href="#">LOGO</a>
-//           </div>
-//           <h3>Sign Up</h3>
-//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-//           <p>Lorem ipsum dolor sit amet. </p>
-//           <div className="mb-3">
-//             <input
-//               type="email"
-//               className="form-control"
-//               id="exampleFormControlInput1"
-//               placeholder="Email address"
-//             />
-//           </div>
-//           <div className="mb-3">
-//             <input
-//               type="email"
-//               className="form-control"
-//               id="exampleFormControlInput1"
-//               placeholder="Password"
-//             />
-//           </div>
-//           <div className="mb-3">
-//             <button className="active">
-//               <FontAwesomeIcon icon={faEnvelope} /> Sign Up
-//             </button>
-//           </div>
-//           <hr />
-//           <div className="mb-2">
-//             <button className="inactive">Sign up with Google</button>
-//           </div>
-//           <div className="mb-2">
-//             <button className="inactive">
-//               <FontAwesomeIcon icon={faEnvelope} /> Sign Up with Apple
-//             </button>
-//           </div>
-//           <p>
-//             Already have an account? <Link to="/LogIn">Log In</Link>
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
