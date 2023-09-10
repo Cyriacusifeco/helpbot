78a5649340b9e4240a871b7c6a6cbeb203ff5208
Charly <icharlieanne@gmail.com>
2023-09-10 13:22:26 +0100

    fix

--- src/component/DashBoard/UserDashboard/UserDashboard.jsx
+++ src/component/DashBoard/UserDashboard/UserDashboard.jsx
@@ -2,7 +2,7 @@ import Profile from '../Profile/Profile';
 import Bot from './Bots/Chatbots/Chatbots';
 import Step1 from './Bots/NewBot/Step-1-onboarding';
 import Step2 from './Bots/NewBot/Step-2-PDF';
-// import Step3 from './Bots/NewBot/Step-3-done';
+import Step3 from './Bots/NewBot/Step-3-done';
 // import Newbot from NewBot'./Bots//NewBot';
 import Inbox from './Inbox/Inbox';
 import { Routes, Route } from 'react-router-dom';
@@ -14,7 +14,7 @@ const UserDashboard = () => {
       {/* <Route path="newbot" element={<Newbot />} /> */}
       <Route path="/create-bot/onboarding" element={<Step1 />} />
       <Route path="/create-bot/pdf-upload" element={<Step2 />} />
-      {/* <Route path="/create-bot/done" element={<Step3 />} /> */}
+      <Route path="/create-bot/done" element={<Step3 />} />
       <Route path="inbox" element={<Inbox />} />
       <Route path="profile" element={<Profile />} />
     </Routes>
