8681d1e9021d46cdf5c2e6963b58ecf08d169bf2
Charly <icharlieanne@gmail.com>
2023-09-20 20:42:16 +0100

    added widget

--- src/component/Widget/ChatWidget.jsx
+++ src/component/Widget/ChatWidget.jsx
@@ -11,11 +11,11 @@ const ChatWidget = () => {
     <>
       <div
         id="chat-icon"
-        className={`icon-closed scale-in-center  ${isOpen ? 'hidden' : ''}`}
+        className="icon-closed scale-in-center"
         style={{
           border: 'none',
-          padding: '10px',
-          backgroundColor: '#2d4059',
+          padding: '8px',
+          backgroundColor: '#ea5455',
           borderRadius: '50%',
           cursor: 'pointer',
           position: 'fixed',
@@ -31,8 +31,8 @@ const ChatWidget = () => {
           fill="#fff"
           viewBox="0 0 24 24"
           strokeWidth="0"
-          height="3em"
-          width="3em"
+          height="2em"
+          width="2em"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path d="M6.45455 19L2 22.5V4C2 3.44772 2.44772 3 3 3H21C21.5523 3 22 3.44772 22 4V18C22 18.5523 21.5523 19 21 19H6.45455ZM7 10C7 12.7614 9.23858 15 12 15C14.7614 15 17 12.7614 17 10H15C15 11.6569 13.6569 13 12 13C10.3431 13 9 11.6569 9 10H7Z"></path>
@@ -44,14 +44,14 @@ const ChatWidget = () => {
             src="http://localhost:3000/"
             style={{
               position: 'fixed',
-              bottom: '20px',
-              right: '20px',
-              width: '300px',
-              height: '400px',
+              bottom: '6rem',
+              right: '3rem',
+              width: '400px',
+              height: '500px',
               backgroundColor: '#fff',
               border: '1px solid #ccc',
-              borderRadius: '5px',
-              boxShadow: '2px 5px rgba(0, 0, 0, 0.5)',
+              borderRadius: '10px',
+              boxShadow: '1px 1px 5px rgba(0, 0, 0, 0.5)',
             }}
           ></iframe>
         </div>
