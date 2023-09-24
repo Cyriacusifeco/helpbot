3d9a89c5b8924bc045ce7f1887dbf7d7db841630
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-09-24 20:42:37 +0000

    adjust widget

--- src/component/DashBoard/UserDashboard/Bots/Settings-theme.jsx
+++ src/component/DashBoard/UserDashboard/Bots/Settings-theme.jsx
@@ -176,11 +176,9 @@ const Theme = ({ botToken }) => {
                             disabled
                           >
                             {/* Embeded code */}
-                            {`<div
-        id="chat-icon"
+                            {`<div id="chat-icon"
         class="icon-closed scale-in-center"
-        onclick="toggleChat()"
-      >
+        onclick="toggleChat()">
         <svg viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M6.45455 19L2 22.5V4C2 3.44772 2.44772 3 3 3H21C21.5523 3 22 3.44772 22 4V18C22 18.5523 21.5523 19 21 19H6.45455ZM7 10C7 12.7614 9.23858 15 12 15C14.7614 15 17 12.7614 17 10H15C15 11.6569 13.6569 13 12 13C10.3431 13 9 11.6569 9 10H7Z"
@@ -196,12 +194,12 @@ const Theme = ({ botToken }) => {
             bottom: 5rem;
             right: 3rem;
             width: 300px;
-            height: 400px;
+            height: 500px;
             background-color: #efdcdc;
             border: 1px solid #ccc;
             border-radius: 5px;
             box-shadow: 2px 5px rgba(0, 0, 0, 0.5);
-          "
+	"
         ></iframe>
       </div>`}
                           </textarea>
